e22cd63f410c6de884a29441e63b04d2b995292e
fatuk <fatukk@gmail.com>
2014-11-29 14:38:40 +0300

    Add genres

--- grunt/watch.js
+++ grunt/watch.js
@@ -1,30 +1,34 @@
 module.exports = {
-    options: {
-        livereload: true
-    },
-    less: {
-        files: 'less/*.less',
-        tasks: ['less', 'autoprefixer'],
-        options: {
-            interrupt: true,
-            livereload: false
-        }
-    },
-    css: {
-        files: 'css/*.css'
-    },
-    html: {
-        files: '*.html'
-    },
-    js: {
-        files: ['js/*.js']
-    },
-    mustache: {
-        files: ['templates/**', 'json/*.json'],
-        tasks: ['mustache_render']
-    },
-    plugins: {
-        files: 'bower_components/**',
-        tasks: ['uglify']
-    }
+	options: {
+		livereload: true
+	},
+	less: {
+		files: 'less/*.less',
+		tasks: ['less', 'autoprefixer'],
+		options: {
+			interrupt: true,
+			livereload: false
+		}
+	},
+	css: {
+		files: 'css/*.css'
+	},
+	html: {
+		files: '*.html'
+	},
+	js: {
+		files: ['js/*.js']
+	},
+	svg: {
+		files: ['svg.src/*.svg'],
+		tasks: ['svg']
+	},
+	mustache: {
+		files: ['templates/**', 'json/*.json'],
+		tasks: ['mustache_render']
+	},
+	plugins: {
+		files: 'bower_components/**',
+		tasks: ['uglify']
+	}
 };
